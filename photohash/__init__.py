66e6506f1afcce2e1843689650a17a758a80bb96
Chris Pickett <chris.pickett@gmail.com>
2013-11-27 08:47:22 -0800

    Increase version to 0.2.0

--- photohash/__init__.py
+++ photohash/__init__.py
@@ -1,1 +1,3 @@
-from photohash import  average_hash, distance, is_look_alike
+from photohash import average_hash, distance, is_look_alike
+
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PhotoHash',
-    version='0.1.0',
+    version='0.2.0',
     author='Chris Pickett',
     author_email='chris.pickett@gmail.com',
     packages=['photohash', ],
