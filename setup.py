e37baa35ebef8efcbe41aab7919044388002ff9f
Chris Pickett <chris.pickett@gmail.com>
2013-11-27 08:49:40 -0800

    Fix license in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='chris.pickett@gmail.com',
     packages=['photohash', ],
     url='https://github.com/bunchesofdonald/photohash',
-    license='LICENSE.txt',
+    license='MIT',
     description='A Python Perceptual Image Hashing Module',
     long_description=open('README.rst').read(),
     install_requires=[
