18c36ac960d2dd11bc2256bdf686b53830b4e237
KK <bebound@gmail.com>
2015-07-25 16:58:55 +1000

    Use with statement to open image

--- photohash/photohash.py
+++ photohash/photohash.py
@@ -8,19 +8,19 @@ def _hamming_distance(string, other_string):
 
 def average_hash(image_path, hash_size=8):
     """ Computes the average hash of the given image. """
+    with open(image_path, 'rb') as f:
+        # Open the image, resize it and convert it to black & white.
+        image = Image.open(f)
+        image = image.resize((hash_size, hash_size), Image.ANTIALIAS).convert('L')
 
-    # Open the image, resize it and convert it to black & white.
-    image = Image.open(image_path)
-    image = image.resize((hash_size, hash_size), Image.ANTIALIAS).convert('L')
+        # Get the average value of a pixel in the image.
+        pixels = list(image.getdata())
+        avg = sum(pixels) / len(pixels)
 
-    # Get the average value of a pixel in the image.
-    pixels = list(image.getdata())
-    avg = sum(pixels) / len(pixels)
-
-    # Compute the hash based on each pixels value compared to the average.
-    bits = "".join(map(lambda pixel: '1' if pixel > avg else '0', pixels))
-    hashformat = "0{hashlength}x".format(hashlength=hash_size * 2)
-    return int(bits, 2).__format__(hashformat)
+        # Compute the hash based on each pixels value compared to the average.
+        bits = "".join(map(lambda pixel: '1' if pixel > avg else '0', pixels))
+        hashformat = "0{hashlength}x".format(hashlength=hash_size * 2)
+        return int(bits, 2).__format__(hashformat)
 
 
 def distance(image_path, other_image_path):
