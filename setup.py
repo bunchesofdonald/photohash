67bd8a1bb96ddb569a86cb4acb1ffc8f7a88a4db
Chris Pickett <chris.pickett@gmail.com>
2015-07-25 07:13:05 -0700

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='PhotoHash',
-    version='0.3.1',
     author='Chris Pickett',
     author_email='chris.pickett@gmail.com',
-    packages=['photohash', ],
-    url='https://github.com/bunchesofdonald/photohash',
-    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+    ],
     description='A Python Perceptual Image Hashing Module',
+    license='MIT',
     long_description=open('README.rst').read(),
+    name='Photohash',
+    packages=find_packages(),
+    url='https://github.com/bunchesofdonald/photohash',
+    version='0.3.1',
     install_requires=[
         'Pillow>=2.1.0',
     ],
